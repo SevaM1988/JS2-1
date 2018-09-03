7a478d5bd9c3a72e4b2c5f0d71c1e1967109b0ac
SevaM1988 <markov.vsevolod@inbox.ru>
2018-09-03 20:05:47 +0300

    menu with submenu for site

--- script.js
+++ script.js
@@ -15,8 +15,9 @@ Container.prototype.render = function () {
 };
 
 Container.prototype.remove = function () {
-    var elem = document.getElementsByTagName('div');
-    elem.remove();
+    var elem = document.getElementById(this.id);
+
+    elem.parentElement.removeChild(elem)
 };
 
 function Menu(id, className, items) {
@@ -64,48 +65,23 @@ MenuItem.prototype.render = function () {
     return li;
 };
 
-function SubMenu(id, className, items) {
-    Menu.call(this, id, className);
-
-    this.items = items;
+function SubMenu(id, className, items, title, href) {
+    Menu.call(this, id, className, items);
+    this.title = title;
+    this.href = href;
 }
 
 SubMenu.prototype = Object.create(Menu.prototype);
 
 SubMenu.prototype.render = function () {
-    var ul = document.createElement('ul');
+    if (this.title && this.href) {
+        var menuItem = new MenuItem(this.href, this.title).render();
+        menuItem.appendChild(Menu.prototype.render.call(this));
 
-    ul.className = this.className;
-    ul.id = this.id;
-
-    this.items.forEach(function (item) {
-        if (item instanceof Menu) {
-            ul.appendChild(item.render());
-        }
-    });
-
-    return ul;
+        return menuItem;
+    } else {
+        return Menu.prototype.render.call(this);
+    }
 };
 
-function SubMenuItem(href, label) {
-    Menu.call(this, '', 'submenu-item');
-
-    this.href = href;
-    this.label = label;
-}
-
-SubMenuItem.prototype = Object.create(Menu.prototype);
-
-SubMenuItem.prototype.render = function () {
-    var li = document.createElement('li');
-    var a = document.createElement('a');
-
-    a.href = this.href;
-    a.textContent = this.label;
-
-    li.appendChild(a);
-    li.className = this.className;
-
-    return li;
-};
 
